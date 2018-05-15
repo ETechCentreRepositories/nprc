7d2f13ca183cd7d9eeb11df4ed08d28b8a90cc95
Ivan Lipotan <ivanlipotan21@gmail.com>
2018-05-15 16:45:59 +0800

    Adding the OCR

--- app/src/main/java/ngeeann/com/redcamp/Splash.java
+++ app/src/main/java/ngeeann/com/redcamp/Splash.java
@@ -6,18 +6,14 @@ import android.content.SharedPreferences;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Build;
 import android.os.Handler;
-import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 
 import com.bumptech.glide.Glide;
-
-import java.util.Objects;
 
 import ngeeann.com.redcamp.Content.MainActivity;
 import ngeeann.com.redcamp.Login.LaunchScreen;
